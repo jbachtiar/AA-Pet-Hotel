2f147d19976ecd8667eccaa2d5deadf82b2af508
Sim Hui Min <huimin.sim.2015@smu.edu.sg>
2018-02-24 19:15:41 +0800

    Create Logbook
    created Logbook

---
+++ Logbook.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hui Min
+ */
+public class Logbook {
+    private ArrayList<LogbookEntry> entries;
+
+    public Logbook(ArrayList<LogbookEntry> entries) {
+        this.entries = entries;
+    }
+
+    public ArrayList<LogbookEntry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(ArrayList<LogbookEntry> entries) {
+        this.entries = entries;
+    }
+    
+    public boolean deleteEntry(Dog dog){
+        for(LogbookEntry e: entries){
+            if(e.getDog().getName().equals(dog.getName())){
+                entries.remove(e);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void addEntry(LogbookEntry e){
+        entries.add(e);
+    }
+}
