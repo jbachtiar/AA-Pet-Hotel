5a40a0f4f43e8118822e7acdf28f9ac04e1a6d6e
Sim Hui Min <huimin.sim.2015@smu.edu.sg>
2018-02-24 19:05:44 +0800

    Created Dog and LogbookEntry files
    Created Dog and LogbookEntry files

---
+++ Dog.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Hui Min
+ */
+public class Dog {
+    private String name;
+    private String size;
+    private int duration;
+
+    public Dog(String name, String size, int duration) {
+        this.name = name;
+        this.size = size;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    
+}
--- LogbookEntry.java
+++ LogbookEntry.java
@@ -9,5 +9,49 @@
  * @author Hui Min
  */
 public class LogbookEntry {
+    private Dog dog;
+    private int roomID;
+    private int startDay;
+    private int endDay;
+
+    public LogbookEntry(Dog dog, int roomID, int startDay, int endDay) {
+        this.dog = dog;
+        this.roomID = roomID;
+        this.startDay = startDay;
+        this.endDay = endDay;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public int getStartDay() {
+        return startDay;
+    }
+
+    public void setStartDay(int startDay) {
+        this.startDay = startDay;
+    }
+
+    public int getEndDay() {
+        return endDay;
+    }
+
+    public void setEndDay(int endDay) {
+        this.endDay = endDay;
+    }
+    
     
 }
