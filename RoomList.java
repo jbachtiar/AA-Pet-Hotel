272ca7cc5559ce8017dd8da5d961bdac06ef28d0
jbachtiar <jeremy.bachtiar@yahoo.com>
2018-02-24 22:42:08 +0800

    added RoomList.java with comment

---
+++ RoomList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+import Room.java;
+
+public class RoomList{
+    private ArrayList<Room> roomList;
+
+    // create 10 rooms
+    public RoomList(){
+        for(int i = 0; i<10; i++){
+            roomList.add(new Room(i));
+        }
+    }
+
+    // retrieve room list
+    public ArrayList<Room> GetRoomList(){
+        return this.roomList;
+    }
+}
